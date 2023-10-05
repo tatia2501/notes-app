7ca25ea9d47093710f561c0bd21152b4cc68e906
tatia2501 <catatat@yandex.ru>
2023-10-05 11:55:43 +0300

    feat: app settings files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { HttpStatus, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { UserModule } from './user/user.module';
+import { APP_FILTER, HttpAdapterHost } from '@nestjs/core';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 @Module({
+  // imports: [UserModule, MarkerModule, AnimationModule],
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useFactory: ({ httpAdapter }: HttpAdapterHost) => {
+        return new PrismaClientExceptionFilter(httpAdapter, {
+          P2000: HttpStatus.BAD_REQUEST,
+          P2025: HttpStatus.NOT_FOUND,
+        });
+      },
+      inject: [HttpAdapterHost],
+    },
+  ],
 })
 export class AppModule {}
