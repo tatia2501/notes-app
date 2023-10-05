543b7a1615b3e60e62017b96e1d9a345a9b5d2b5
tatia2501 <catatat@yandex.ru>
2023-10-06 02:08:21 +0300

    feat: init page button

--- public/scripts/account.js
+++ public/scripts/account.js
@@ -1,7 +1,11 @@
+const init_page_btn = document.getElementById('init_page_btn');
 const copy_code_btn = document.getElementById('copy_code_btn');
 const logout_btn = document.getElementById('logout_btn');
 const delete_account_btn = document.getElementById('delete_account_btn');
 
+init_page_btn.addEventListener('click', async () => {
+  window.location.href = '/notes';
+});
 copy_code_btn.addEventListener('click', async () => {
   await navigator.clipboard.writeText(localStorage.getItem('notes_code'));
 });
